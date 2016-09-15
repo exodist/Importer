0fa2eaeab483b0fa87cddff7b63e6a61b4a9a3fc
Chad Granum <exodist7@gmail.com>
2016-09-15 12:08:42 -0700

    Automated Version Bump

--- lib/Importer.pm
+++ lib/Importer.pm
@@ -2,7 +2,7 @@ package Importer;
 use strict qw/vars subs/; # Not refs!
 use warnings; no warnings 'once';
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 my %SIG_TO_SLOT = (
     '&' => 'CODE',
